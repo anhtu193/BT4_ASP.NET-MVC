bd2889590fe26747b796c8144363412f760ddf94
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2024-10-10 19:46:53 +0700

    first commit

---
+++ BT4_ASP.NET MVC/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using BT4_ASP.NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT4_ASP.NET_MVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext("Data Source=LAPTOP-H3A07V47;Initial Catalog=QuanLySanPham;Integrated Security=True");
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ BT4_ASP.NET MVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using BT4_ASP.NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT4_ASP.NET_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext("Data Source=LAPTOP-H3A07V47;Initial Catalog=QuanLySanPham;Integrated Security=True");
+            List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);
+        }
+    }
+}
